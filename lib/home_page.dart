a3ba440e63fb7066f2653b0061892913a1f26853
oba-takumi <oba_takumi@nanairo-inc.jp>
2023-07-14 16:29:33 +0900

    登録した収支を表示する機能を追加

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'app_drawer.dart';
 import 'consts.dart';
+import 'db_helper.dart';
 import 'edit_page.dart';
+import 'records.dart';
 
 /// ホーム画面
 class HomePage extends StatefulWidget {
-  /// コンストラクタ
-  const HomePage({super.key});
+  /// コンストラクタ - スクロール位置保持のために PageStorageKey を指定
+  const HomePage() : super(key: const PageStorageKey<String>('home'));
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -18,7 +20,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(Consts.appTitle)),
-      body: Container(),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: DbHelper.selectRecords(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<List<Records>> snapshot,
+        ) {
+          // データが準備できていない場合はプログレスアイコンを表示
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            itemCount: snapshot.data?.length ?? 0,
+            itemBuilder: (context, int index) {
+              final record = snapshot.data![index];
+              return ListTile(
+                leading: Icon(record.category.icon),
+                title: Text(record.title ?? '---'),
+                subtitle: Text(record.toString()),
+                onTap: () async {
+                  // 編集画面に遷移
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => EditPage(record)),
+                  );
+
+                  // 一覧を更新
+                  setState(() {});
+                },
+              );
+            },
+          );
+        },
+      ),
+      // 新規追加ボタン
       floatingActionButton: FloatingActionButton(
         tooltip: '新規追加',
         onPressed: () async {
@@ -33,7 +71,6 @@ class _HomePageState extends State<HomePage> {
         },
         child: const Icon(Icons.add),
       ),
-      drawer: const AppDrawer(),
     );
   }
 }
